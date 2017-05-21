f712347d6daefcd8e26de44e3e2e5239beb7c808
Philip Tenn <ptenn@users.noreply.github.com>
2017-05-20 23:50:29 -0400

    Remove diagnostics, this was causing issues.

--- GeneratePdf_WCFService/WebRole.cs
+++ GeneratePdf_WCFService/WebRole.cs
@@ -9,17 +9,5 @@ namespace GeneratePdf_WCFService
 {
     public class WebRole : RoleEntryPoint
     {
-        public override bool OnStart()
-        {
-            // To enable the AzureLocalStorageTraceListner, uncomment relevent section in the web.config  
-            DiagnosticMonitorConfiguration diagnosticConfig = DiagnosticMonitor.GetDefaultInitialConfiguration();
-            diagnosticConfig.Directories.ScheduledTransferPeriod = TimeSpan.FromMinutes(1);
-            diagnosticConfig.Directories.DataSources.Add(AzureLocalStorageTraceListener.GetLogDirectory());
-
-            // For information on handling configuration changes
-            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
-
-            return base.OnStart();
-        }
     }
 }
