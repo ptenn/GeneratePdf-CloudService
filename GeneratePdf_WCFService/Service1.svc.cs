ebc53b3bee3d03ebf0075e003c90c7a6c5c4dcf0
Philip Tenn <ptenn@users.noreply.github.com>
2017-05-21 20:23:27 -0400

    Support Excel and PowerPoint.

--- GeneratePdf_WCFService/Service1.svc.cs
+++ GeneratePdf_WCFService/Service1.svc.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.ServiceModel.Web;
-using System.Text;
 using Syncfusion.DocToPDFConverter;
 using Syncfusion.DocIO.DLS;
-using Syncfusion.DocIO;
 using Syncfusion.Pdf;
 using System.IO;
+using Syncfusion.ExcelChartToImageConverter;
+using Syncfusion.ExcelToPdfConverter;
+using Syncfusion.Presentation;
+using Syncfusion.PresentationToPdfConverter;
+using Syncfusion.XlsIO;
 
 namespace GeneratePdf_WCFService
 {
@@ -17,22 +15,91 @@ namespace GeneratePdf_WCFService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GeneratePdf(string value)
+        public string GeneratePdf(string value, string type)
         {
-            //Loading word document
-            WordDocument document = new WordDocument(new MemoryStream(Convert.FromBase64String(value)), FormatType.Docx);
-            DocToPDFConverter conv = new DocToPDFConverter();
-            //Converts the word document to pdf
-            PdfDocument pdf = conv.ConvertToPDF(document);
-            MemoryStream stream = new MemoryStream();
-            //Saves the Pdf document to stream
-            pdf.Save(stream);
-            //Sets the stream position
-            stream.Position = 0;
-            pdf.Close();
-            document.Close();
-            //Returns the stream data as Base 64 string
-            return Convert.ToBase64String(stream.ToArray());
+            PdfDocument pdf;
+            MemoryStream stream;
+            switch (type)
+            {
+                // Word
+                case ".doc":
+                case ".docx":
+                case ".docm":
+                case ".dotm":
+                case ".rtf":
+                case ".dot":
+                case ".dotx":
+                    //Loading word document
+                    WordDocument document = new WordDocument(new MemoryStream(Convert.FromBase64String(value)));
+                    DocToPDFConverter conv = new DocToPDFConverter();
+                    //Converts the word document to pdf
+                    pdf = conv.ConvertToPDF(document);
+                    stream = new MemoryStream();
+                    //Saves the Pdf document to stream
+                    pdf.Save(stream);
+                    //Sets the stream position
+                    stream.Position = 0;
+                    pdf.Close();
+                    document.Close();
+                    //Returns the stream data as Base 64 string
+                    return Convert.ToBase64String(stream.ToArray());
+                // Excel
+                case ".xlsx":
+                case ".xlsm":
+                case ".xlsb":
+                case ".xls":
+                case ".xltx":
+                case ".csv":
+                    ExcelEngine excelEngine = new ExcelEngine();
+                    IApplication application = excelEngine.Excel;
+
+                    application.ChartToImageConverter = new ChartToImageConverter();
+
+                    IWorkbook workbook = application.Workbooks.Open(new MemoryStream(Convert.FromBase64String(value)), ExcelOpenType.Automatic);
+
+                    ExcelToPdfConverter convert = new ExcelToPdfConverter(workbook);
+                    pdf = convert.Convert();
+                    stream = new MemoryStream();
+
+                    //Saves the Pdf document to stream
+                    pdf.Save(stream);
+                    //Sets the stream position
+                    stream.Position = 0;
+                    pdf.Close();
+                    workbook.Close();
+                    //Returns the stream data as Base 64 string
+                    return Convert.ToBase64String(stream.ToArray());
+                // PowerPoint
+                case ".pptx":
+                case ".pptm":
+                case ".ppt":
+                case ".potx":
+                case ".ppsx":
+                case ".potm":
+                case ".pot":
+                case ".ppsm":
+                case ".pps":
+                    //Opens a PowerPoint Presentation
+                    IPresentation presentation = Presentation.Open(new MemoryStream(Convert.FromBase64String(value)));
+
+                    //Creates an instance of ChartToImageConverter and assigns it to ChartToImageConverter property of Presentation
+                    presentation.ChartToImageConverter = new Syncfusion.OfficeChartToImageConverter.ChartToImageConverter();
+
+                    //Converts the PowerPoint Presentation into PDF document
+                    pdf = PresentationToPdfConverter.Convert(presentation);
+
+                    stream = new MemoryStream();
+                    //Saves the PDF document
+                    pdf.Save(stream);
+
+                    //Sets the stream position
+                    stream.Position = 0;
+                    pdf.Close();
+                    presentation.Close();
+                    //Returns the stream data as Base 64 string
+                    return Convert.ToBase64String(stream.ToArray());
+            }
+            return null;
         }
     }
 }
