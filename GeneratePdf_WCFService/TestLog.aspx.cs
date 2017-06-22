56555d7157177780c11d008b867ece5dd2f4fa5f
Philip Tenn <ptenn@users.noreply.github.com>
2017-06-22 13:28:29 -0400

    Commit work in progress

---
+++ GeneratePdf_WCFService/TestLog.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using log4net;
+
+namespace GeneratePdf_WCFService
+{
+    public partial class TestLog : System.Web.UI.Page
+    {
+
+        private static ILog logger = LogManager.GetLogger(typeof(TestLog));
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
